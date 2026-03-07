bcd3330b514e0e6cd258f38b2355ccec086b275d
José Cruz <98415031+jacruz061175@users.noreply.github.com>
2026-03-07 00:25:33 -0300

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,11 @@ API_SECRET = "Tsl0fDh9P8Rk2ftzjy7Novs4p1weyCDzC5tYNnReCZHsPVRZGwlIcvufxQyfSL6k"
 
 # Monedas que operás (las mismas de tus bots/sh)
 PNL_SYMBOLS = ["ADAUSDT", "BNBUSDT", "BTCUSDT", "ETHUSDT"]
-RENDER_UPDATE_URL = "https://monitor-alpha.onrender.com//update"
+RENDER_UPDATE_URL = "https://monitor-alpha.onrender.com/update"
 
 RENDER_UPDATE_TOKEN = "Mateo0712*"
 # Si querés cambiar el refresh global del monitor
 PNL_REFRESH_MINUTES = 5
+
 
 
